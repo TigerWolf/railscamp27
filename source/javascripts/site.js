38070e6cc89d5838348845cdf2f637dddc1b66ab
Matt Patterson <matt.patterson94@gmail.com>
2020-02-15 21:09:55 +1100

    Set up most of new homepage

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,16 @@
 // This is where it all goes :)
+$('.slider').slick({
+  slidesToShow: 1,
+  arrows: false,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+      }
+    },
+  ]
+});
